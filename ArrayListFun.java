3e0645b9e35c5187c3ef3b86a526d6b43b5eb179
Fred Morrison <fpmorrison@verizon.net>
2020-02-06 09:27:38 -0500

    cosmetic changes

--- ArrayListFun.java
+++ ArrayListFun.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 public class ArrayListFun {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> grades = new ArrayList<Integer>( Arrays.asList(99, 88, 92) );
+		ArrayList<Integer> grades = new ArrayList<Integer>(Arrays.asList(99, 88, 92));
 		System.out.println(grades);
-
 	}
-
 }
