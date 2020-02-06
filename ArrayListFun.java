f2ad0ff11b0268f381d1eb41e444b033fa520725
Fred Morrison <fpmorrison@verizon.net>
2020-02-06 09:25:36 -0500

    example ArrayList initializer
    ArrayList initializer

---
+++ ArrayListFun.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListFun {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> grades = new ArrayList<Integer>( Arrays.asList(99, 88, 92) );
+		System.out.println(grades);
+
+	}
+
+}
