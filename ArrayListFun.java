24349119c8d4e07cf4f4fb978470008df7287ee6
Fred Morrison <fpmorrison@verizon.net>
2020-02-07 20:28:45 -0500

    refactor

--- ArrayListFun.java
+++ ArrayListFun.java
@@ -1,10 +1,15 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class ArrayListFun {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> grades = new ArrayList<Integer>(Arrays.asList(99, 88, 92));
 		System.out.println(grades);
+		
+		List<String> students = List.of("Mary","Mahindra");
+		System.out.println(students);
+		
+		List<Integer> listNumbers = List.of(1, 2, 3, 4, 5, 6);
+		
 	}
 }
